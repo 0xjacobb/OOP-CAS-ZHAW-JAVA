5fd693792f58496f4bfb1fec46ee7a144651bf55
Marc Steiner <spare@gmx.chr>
2018-11-09 17:32:35 +0100

    added new project code

--- OOP-CAS-ZHAW-JAVA/src/writeStringToFile/WriteFile.java
+++ OOP-CAS-ZHAW-JAVA/src/writeStringToFile/WriteFile.java
@@ -1,8 +1,11 @@
 package writeStringToFile;
 
+import java.io.File;
+
 public class WriteFile {
 
 	public static void main(String[] args) {
+		File file = new File("src/files/datei.txt");
 		// TODO Auto-generated method stub
 
 	}
