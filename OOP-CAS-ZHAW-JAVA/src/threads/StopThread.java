4dfe3a2b6a9cbd50f75e95fa64efedcbfd77a9a2
Marc Steiner <spare@gmx.chr>
2018-11-16 20:14:31 +0100

    aded code

--- OOP-CAS-ZHAW-JAVA/src/threads/StopThread.java
+++ OOP-CAS-ZHAW-JAVA/src/threads/StopThread.java
@@ -16,11 +16,23 @@ public class StopThread extends Thread {
 	}
 	
 	public void run() {
-		
+		int count = 0;
+		while(!isStopped()) {
+			count ++;		
+			System.out.println(count);
+		}
+		System.out.println("Thread ended jetzt");
 	}
 
 	public static void main(String[] args) {
 		StopThread st = new StopThread();
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		st.stopThread();
+		System.out.println("Eine Meldung");
 
 	}
 
